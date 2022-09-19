aa686a0f822b5f253597edbdc6db8435cc63c6dd
Cansu Öztaş <fcansuoztas@gmail.com>
2022-09-19 11:15:25 +0300

    finish

--- app.js
+++ app.js
@@ -1,45 +1,74 @@
 const input = document.querySelector("#input");
 const btn = document.querySelector("#btn");
 const todo = document.querySelector("#todo");
+// const size = document.querySelector("#size");
 const remove = `<button>Remove</button>`;
-const altdiv = document.getElementById("altdiv");
+const altdiv = document.querySelector(".altdiv");
+const icon = document.getElementById("icon");
 
 let day = new Date().getDate();
 let month = new Date().getMonth() + 1;
 let year = new Date().getFullYear();
 
-//? *********onload******
+window.onload = function () {
+  input.focus();
+};
 
-// window.addEventListener("load", () => {
-//   //baslangıç
-//   const todos = localStorage.getItem("todos");
-//   if (!todos) {
-//     localStorage.setItem("todos", JSON.stringify([]));
-//     console.log(todos);
-//   } else {
-//     let datalist = JSON.parse(localStorage.getItem("todos")); // dataliste eğer json parse içi doluysa yani true ise aktar ama yoksa [] yi aktar demek || oluyor.
+window.addEventListener("load", () => {
+  let data = localStorage.getItem("toDos");
 
-//     datalist.forEach((e) => {
-//       //? her bir todo objesini destructor yaptık
-//       const { id, text, date } = e;
-//       let element = document.createElement("div");
-//       element.classList.add("altdiv");
-//       element.innerHTML = `<input class="box" type = "checkbox"   />
-//     <p>${text}</p><div class="date">${date}<button class = "rmvBtn"> REMOVE</button></div>`;
-//       list.appendChild(element);
-//     });
-//   }
+  if (data === null) {
+    localStorage.setItem("toDos", JSON.stringify([]));
+  } else {
+    let data = JSON.parse(localStorage.getItem("toDos"));
+    // console.log(data);
+    data.forEach((element) => {
+      starter(element);
+    });
+  }
+});
+
+// window.addEventListener("resize",()=>{
+//     size.innerHTML = window.innerWidth
+//     console.log(window.innerWidth)
 // })
 
-btn.addEventListener("click", () => {
+const starter = (element) => {
+  // console.log(element)
+  todo.innerHTML += `<div class="altdiv" id="altdiv">
+    <i  id="icon" class="fa-regular fa-square-full checked icon"></i>
+    <p class="par">${element.text}</p>
+    <div class="günler">${element.day}.${element.month}.${element.year}  
+    <button class="remove">remove</button></div></div>`;
+};
+
+const adding = () => {
   if (!input.value) {
     alert("birşey gir");
   } else {
-    todo.innerHTML += `<div class="altdiv" id="altdiv"><input class=box type="checkbox"><p>${input.value}</p><div class="günler">${day}.${month}.${year}  <button class= "remove1">remove</button></div></div>`;
+    todo.innerHTML += `<div class="altdiv" id="altdiv">
+    <i  id="icon" class="fa-regular fa-square-full checked icon"></i>
+    <p class="par">${input.value}</p>
+    <div class="günler">
+    ${day}.${month}.${year}  
+    <button class="remove">remove</button></div>
+    </div>`;
+
+    let dataTemplate = {
+      text: input.value,
+      day: day,
+      month: month,
+      year: year,
+    };
+    // console.log(dataTemplate)
+    let toDos = JSON.parse(localStorage.getItem("toDos"));
+    toDos.push(dataTemplate);
+    localStorage.setItem("toDos", JSON.stringify(toDos));
   }
   input.value = "";
-});
-//***********enter */
+};
+
+btn.addEventListener("click", adding);
 
 input.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
@@ -47,41 +76,41 @@ input.addEventListener("keydown", (e) => {
   }
 });
 
-// // *************remove*****
-
 todo.addEventListener("click", (e) => {
-  if (e.target.classList.contains("remove1")) {
+  if (
+    e.target.classList.contains("remove") &&
+    e.target.parentElement.parentElement.classList.contains("checked")
+  ) {
     e.target.parentElement.parentElement.remove();
+
+    // console.log(e.target.parentElement.parentElement.lastChild.previousElementSibling.innerHTML);
+
+    let toDos = JSON.parse(localStorage.getItem("toDos"));
+    let removedList = toDos.filter(
+      (element) =>
+        element.text !==
+        e.target.parentElement.parentElement.lastChild.previousElementSibling
+          .innerHTML
+    );
+    console.log(removedList);
+    localStorage.setItem("toDos", JSON.stringify(removedList));
+  } else if (!e.target.classList.contains("remove")) {
+  } else {
+    alert("görevi tamamlayınız");
   }
 });
 
-//******line through */
-// todo.addEventListener("click", (e) => {
-//   if (e.target.classList.contains("box")) {
-//     e.target.nextElementSibling.style.textDecoration = "line-through";
-//   }
-// });
+todo.addEventListener("click", (e) => {
+  if (e.target.classList.contains("icon")) {
+    if (e.target.parentElement.classList.contains("checked")) {
+      e.target.classList.remove("fa-solid", "fa-check");
+      e.target.classList.add("fa-regular", "fa-square-full");
 
-// todo.addEventListener("click",(e)=>{
-//     let sayac = 0
-//     for(let i=0 ; i<1000 ; i++)
-//     if(e.target.classList.contains("box")){
-//         sayac +=1;
-//     if(sayac)
-//     }
-// })
-
-/* REMOVE, CHECK */
-for (i = 0; i < 100; i++) {
-  let sayac = 1;
-  todo.addEventListener("click", (e) => {
-    if (e.target.classList.contains("box")) {
-      if (sayac % 2 == 1) {
-        e.target.nextElementSibling.style.textDecoration = "line-through";
-      } else if (sayac % 2 == 0) {
-        e.target.nextElementSibling.style.textDecoration = "none";
-      }
-      sayac += 1;
+      e.target.parentElement.classList.remove("checked");
+    } else {
+      e.target.parentElement.classList.add("checked");
+      e.target.classList.remove("fa-regular", "fa-square-full");
+      e.target.classList.add("fa-solid", "fa-check");
     }
-  });
-}
+  }
+});
